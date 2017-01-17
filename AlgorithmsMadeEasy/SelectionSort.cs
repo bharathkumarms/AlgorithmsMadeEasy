012a2ac6875253994d934d50477e09704633b356
Bharathkumar V <bharathkumar.vaitheeswaran@adp.com>
2017-01-17 14:39:33 +0530

    Solved Selection Sort Problem

---
+++ AlgorithmsMadeEasy/SelectionSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsMadeEasy
+{
+    class SelectionSort
+    {
+        public void SelectionSortMethod()
+        {
+            var input = System.Console.ReadLine();
+            string[] sInput = input.Split(' ');
+            int[] iInput = Array.ConvertAll(sInput, int.Parse);
+
+            for (int i = 0; i < iInput.Length; i++)
+            {
+                int minIndex = 0;
+                bool flagChanged = false;
+                for (int j = i; j < iInput.Length - 1; j++)
+                {
+                    if (iInput[j] > iInput[j + 1])
+                    {
+                        minIndex = j + 1;
+                        flagChanged = true;
+                    }
+                }
+                if (flagChanged)
+                {
+                    int temp = iInput[i];
+                    iInput[i] = iInput[minIndex];
+                    iInput[minIndex] = temp;
+                }
+            }
+        }
+    }
+}
