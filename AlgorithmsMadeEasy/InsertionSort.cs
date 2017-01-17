87be0ea4febb029908ccee50ab8ba5b3c17d86b1
Bharathkumar V <bharathkumar.vaitheeswaran@adp.com>
2017-01-17 17:12:39 +0530

    Solved Insertion Sort Problem

---
+++ AlgorithmsMadeEasy/InsertionSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsMadeEasy
+{
+    class InsertionSort
+    {
+        public void InsertionSortMethod()
+        {
+            var input = System.Console.ReadLine();
+            string[] sInput = input.Split(' ');
+            int[] iInput = Array.ConvertAll(sInput, int.Parse);
+
+            int value = 0;
+
+            for (int i = 1; i < iInput.Length; i++)
+            {
+                value = iInput[i];
+
+                int sortedCount = i - 1;
+
+                while (sortedCount >= 0 && iInput[sortedCount] > value)
+                {
+                    iInput[sortedCount + 1] = iInput[sortedCount];
+                    sortedCount--;
+                }
+                iInput[sortedCount + 1] = value;
+            }
+
+            for (int k = 0; k < iInput.Length; k++)
+            {
+                Console.Write(iInput[k] + " ");
+            }
+            Console.Read();
+        }
+    }
+}
+
+/*
+ * Sample Input
+6 5 3 2 8
+*/
