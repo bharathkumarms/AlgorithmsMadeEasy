8f99e6a77eb65e4c2a5b0013d5ca2d54caf686de
Bharathkumar V <bharathkumar.vaitheeswaran@adp.com>
2017-01-18 14:17:25 +0530

    Solved Merge Sort Problem

---
+++ AlgorithmsMadeEasy/MergeSort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsMadeEasy
+{
+    class MergeSort
+    {
+        public void MergeSortMethod()
+        {
+            var input = System.Console.ReadLine();
+            string[] sInput = input.Split(' ');
+            int[] numbers = Array.ConvertAll(sInput, int.Parse);
+            int len = numbers.Length;
+            MergeSort_Recursive(numbers, 0, len - 1);
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        static public void MergeSort_Recursive(int[] numbers, int left, int right)
+        {
+            int mid;
+
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                MergeSort_Recursive(numbers, left, mid);
+                MergeSort_Recursive(numbers, (mid + 1), right);
+
+                DoMerge(numbers, left, (mid + 1), right);
+            }
+        }
+
+        static public void DoMerge(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[numbers.Length];
+            int i, left_end, num_elements, tmp_pos;
+
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+
+            while ((left <= left_end) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[tmp_pos++] = numbers[left++];
+                else
+                    temp[tmp_pos++] = numbers[mid++];
+            }
+
+            while (left <= left_end)
+                temp[tmp_pos++] = numbers[left++];
+
+            while (mid <= right)
+                temp[tmp_pos++] = numbers[mid++];
+
+            for (i = 0; i < num_elements; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+    }
+}
+
+/*
+ * Sample Input
+6 5 3 2 8
+*/
