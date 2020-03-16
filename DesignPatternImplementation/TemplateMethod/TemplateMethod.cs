863df6c0feab1aaba6cf8cb15f77e569bc2f6a65
Bharath V <bharathkumar.vaitheeswaran@adp.com>
2020-03-16 18:41:01 +0530

    Template method pattern

---
+++ DesignPatternImplementation/TemplateMethod/TemplateMethod.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternImplementation.TemplateMethod
+{
+    public abstract class OrderTemplate
+    {
+        public abstract void Choose();
+        public abstract void Pay();
+        public abstract void Deliver();
+
+        public bool isMember;
+
+        public void Discount()
+        {
+            Console.WriteLine("Additional 10% discount processed for member");
+        }
+
+        public void Process(bool isMember)
+        {
+            Choose();
+            if (isMember)
+            {
+                Discount();
+            }
+            Pay();
+            Deliver();
+        }
+    }
+
+    public class Online : OrderTemplate
+    {
+        public override void Choose()
+        {
+            Console.WriteLine("Product selected online.");
+        }
+
+        public override void Deliver()
+        {
+            Console.WriteLine("Product delivered(online).");
+        }
+
+        public override void Pay()
+        {
+            Console.WriteLine("Paid online.");
+        }
+    }
+
+    public class Offline : OrderTemplate
+    {
+        public override void Choose()
+        {
+            Console.WriteLine("Product selected offline.");
+        }
+
+        public override void Deliver()
+        {
+            Console.WriteLine("Product delivered(offline).");
+        }
+
+        public override void Pay()
+        {
+            Console.WriteLine("Paid offline.");
+        }
+    }
+}
+/*
+    var onlineOrder = new Online();
+    onlineOrder.Process(true);
+
+    Console.WriteLine();
+
+    var offlineOrder = new Offline();
+    offlineOrder.Process(false);
+
+    Console.ReadLine();
+*/
