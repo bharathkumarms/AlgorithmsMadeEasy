77a9fdbb95dcc2885e412e7070de21db50e7f16b
Bharath V <bharathkumar.vaitheeswaran@adp.com>
2020-06-07 08:03:14 +0530

    Prototype Design Pattern

---
+++ DesignPatternImplementation/Prototype/Prototype.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternImplementation.Prototype
+{
+    public class Language : ICloneable
+    {
+        private List<string> _languages = new List<string>();
+
+        public void Load()
+        {
+            //This is a database operation
+            _languages.Add("C#");
+            _languages.Add("Java");
+            _languages.Add("Groovy");
+        }
+
+        public List<string> Get()
+        {
+            return _languages;
+        }
+
+        public void Print()
+        {
+            foreach(var l in _languages)
+            {
+                Console.Write(l + " ");
+            }
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
+
+/*
+    var language = new Language();
+    language.Load();
+
+    var cloned = (Language) language.Clone();
+    var clonedList = cloned.Get();
+    clonedList.Add("Python");
+
+    cloned.Print();
+
+    Console.ReadLine();
+*/
--- DesignPatternImplementation/Singleton/Singleton.cs
+++ DesignPatternImplementation/Singleton/Singleton.cs
@@ -8,36 +8,36 @@ namespace DesignPatternImplementation.SingletonImpl
 {
     public sealed class Singleton
     {
-        Singleton()
-        {
-        }
+        Singleton() { }
+
         private static readonly object padlock = new object();
         private static Singleton instance = null;
+
         public static Singleton Instance
         {
             get
             {
                 lock (padlock)
                 {
-                    if (instance == null)
+                    if(instance == null)
                     {
                         instance = new Singleton();
-                        Console.WriteLine("Object Created.");
+                        Console.WriteLine("Object Created");
                     }
                     else
                     {
-                        Console.WriteLine("Created Object Created.");
+                        Console.WriteLine("Returned Created Object");
                     }
+
                     return instance;
                 }
             }
         }
     }
 }
+
 /*
     var instance = Singleton.Instance;
     var instance1 = Singleton.Instance;
     var instance2 = Singleton.Instance;
-
-    Console.ReadLine();
 */
